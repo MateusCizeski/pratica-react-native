e186545e5f8edf932c60dbf2bde77ffb40ea4fbb
Mateus Cizeski <mateuscizeski@gmail.com>
2024-07-07 13:33:23 -0300

    prática com useeffect e ciclos de vida de um componente.

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    console.log('montou')
+  }, [contador])
+
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <Button title='Aumentar' onPress={() => setContador(contador + 1)} />
+        <Text style={{ fontSize: 30 }}>{contador}</Text>
+      <Button title='Diminuir' onPress={() => setContador(contador - 1)} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
